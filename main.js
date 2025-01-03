3b2fca82ddfb9a571faa8eb9f466421646a3f0e0
Thomas Trivino <th992984@ucf.edu>
2025-01-03 02:08:56 -0500

    Created basic function

--- main.js
+++ main.js
@@ -1,31 +1,39 @@
-// main.js
+const { chromium } = require("playwright");
+const readline = require("readline");
 
-// Function to add two numbers
-function add(a, b) {
-  return a + b;
-}
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
-// Function to subtract two numbers
-function subtract(a, b) {
-  return a - b;
-}
+const userAgentStrings = [
+  "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.2227.0 Safari/537.36",
+  "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
+  "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.3497.92 Safari/537.36",
+  "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36"
+];
 
-// Function to multiply two numbers
-function multiply(a, b) {
-  return a * b;
-}
+rl.question("Please enter the address: ", async address => {
+  // Launch Chromium browser instance
+  const browser = await chromium.launch((headless = false));
 
-// Function to divide two numbers
-function divide(a, b) {
-  if (b === 0) {
-    return "Error: Division by zero";
-  }
-  return a / b;
-}
+  // Create a new browser context with a randomly selected user agent string
+  const context = await browser.newContext({
+    userAgent:
+      userAgentStrings[Math.floor(Math.random() * userAgentStrings.length)]
+  });
 
-// Test the functions
-console.log("Add 2 + 3: ", add(2, 3)); // 5
-console.log("Subtract 5 - 2: ", subtract(5, 2)); // 3
-console.log("Multiply 3 * 4: ", multiply(3, 4)); // 12
-console.log("Divide 10 / 2: ", divide(10, 2)); // 5
-console.log("Divide 10 / 0: ", divide(10, 0)); // Error: Division by zero
+  const page = await context.newPage();
+
+  await page.goto("https://www.zillow.com/");
+
+  await page.waitForSelector('input[type="text"][aria-label="Search"]');
+  await page.fill('input[type="text"][aria-label="Search"]', address);
+  await page.press('input[type="text"][aria-label="Search"]', "Enter");
+
+  await page.waitForSelector(".search-results");
+  console.log(page.url);
+
+  await browser.close();
+  rl.close();
+});
